52f0de2446c737d27041b0ddc2b43f2671adc012
ShonDevelopment <106615918+ShonDevelopment@users.noreply.github.com>
2023-06-08 03:51:34 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+gsap.from(".text", { duration: 1.5, opacity: 0, x: -100 });
+gsap.from(".sub-text", { duration: 1.5, opacity: 0, x: -100 });
+gsap.from(".box-container", { duration: 1.5, opacity: 0, x: -100 });
