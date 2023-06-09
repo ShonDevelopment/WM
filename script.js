b8281dc0fa782482fc180497562a9e9ff3e78fa8
ShonDevelopment <106615918+ShonDevelopment@users.noreply.github.com>
2023-06-09 17:44:11 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,40 @@
-// gsap.from(".text", { duration: 1.5, opacity: 0, x: -100 });
-// gsap.from(".sub-text", { duration: 1.5, opacity: 0, x: -100 });
-// gsap.from(".box-container", { duration: 1.5, opacity: 0, x: -100 });
+"use strict";
+
+window.addEventListener("scroll", function () {
+  var navbar = document.getElementById("navbar");
+  if (window.scrollY > 0) {
+    navbar.classList.add("navbar-scrolled");
+  } else {
+    navbar.classList.remove("navbar-scrolled");
+  }
+});
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
+
+const sections = document.querySelectorAll("section");
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("is-visible");
+    } else {
+      entry.target.classList.remove("is-visible");
+    }
+  });
+});
+
+sections.forEach((section) => {
+  observer.observe(section);
+});
+
+function handlePayment(productName, price) {
+  
+}
