eb267a54008bf31ec732a764de6efe1a997fb901
ShonDevelopment <106615918+ShonDevelopment@users.noreply.github.com>
2023-06-11 09:26:26 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 "use strict";
 
-function handlePayment(productName, price) {
-  alert(`you buyed ${productName} for ${price} dollars.`)
-}
+document.querySelectorAll('nav a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    const target = document.querySelector(this.getAttribute('href'));
+    const offset = 100;
+
+    if (target) {
+      window.scrollTo({
+        top: target.offsetTop - offset,
+        behavior: 'smooth'
+      });
+    }
+  });
+});
+
+// function handlePayment(productName, price) {
+//   alert(`you buyed ${productName} for ${price} dollars.`)
+// }
